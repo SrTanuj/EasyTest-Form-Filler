ac0b5fd9120c08677534381f401e14d0068973e5
Tanuj Jain <rowdytanuj18@gmail.com>
2024-04-02 14:32:49 +0530

    updated for range and date fields plus optimization

--- content.js
+++ content.js
@@ -11,21 +11,25 @@ async function fillFormFields() {
     const fieldNodeName = field.nodeName;
     const fieldName = field.name.toLowerCase();
     const fieldLength = (field.maxLength > 0) ? field.maxLength : 10;
-    if (fieldType === 'text' || fieldType === 'email' || fieldType === 'password' || fieldType === 'tel' || fieldType === 'number' || fieldType === 'color' || fieldNodeName === 'TEXTAREA') {
-      field.value = generateRandomData(fieldType, fieldLength, fieldName);
+    const fieldMax = field.max;
+    const fieldMin = field.min;
+
+    const validFieldTypes = ['text', 'email', 'password', 'tel', 'number', 'color', 'range', 'date'];
+    if (validFieldTypes.includes(fieldType) || fieldNodeName === 'TEXTAREA') {
+      field.value = generateRandomData(fieldType, fieldLength, fieldName, fieldMax, fieldMin);
     }
     else if (fieldType === 'checkbox' || fieldType === 'radio') {
       field.checked = Math.random() > 0.5;
     }
-    else if (field.nodeName === 'SELECT') {
+    else if (fieldNodeName === 'SELECT') {
       const options = field.options;
-      const randomIndex = Math.floor(Math.random() * options.length);
+      const randomIndex = Math.floor(Math.random() * (options.length - 1)) + 1;
       options[randomIndex].selected = true;
     }
   });
 }
 
-function generateRandomData(fType, fLength, fName) {
+function generateRandomData(fType, fLength, fName, fMax, fMin) {
 
   switch (fType) {
     case 'text':
@@ -58,6 +62,12 @@ function generateRandomData(fType, fLength, fName) {
     case 'color':
       var randomData = "#" + randomString(6, ["A", "F"], ["0", "9"]);
       break;
+    case 'range':
+      var randomData = Math.floor(Math.random() * (fMax - fMin + 1) + fMin);
+      break;
+    case 'date':
+      var randomData = generateRandomDate(fMin, fMax);
+      break;
     default:
       const wordsArray = [
         'sunflowers', 'bloom', 'brightly', 'in', 'the', 'warm', 'summer', 'breeze', 'under', 'a', 'clear', 'blue', 'sky', 'near', 'the', 'rolling', 'green', 'hills'
@@ -76,13 +86,20 @@ function generateRandomData(fType, fLength, fName) {
   return randomData;
 }
 
+function generateRandomDate(minDateStr, maxDateStr) {
+  let minDate = minDateStr ? new Date(minDateStr) : new Date(0);
+  let maxDate = maxDateStr ? new Date(maxDateStr) : new Date();
+  let randomTime = minDate.getTime() + Math.random() * (maxDate.getTime() - minDate.getTime());
+  return new Date(randomTime).toISOString().split('T')[0];
+}
+
 function randomString(length, ...ranges) {
   var str = "";
   while(length--) {
-    var i = Math.floor(Math.random() * ranges.length);
-    var min = ranges[i][0].charCodeAt(0),
+    let i = Math.floor(Math.random() * ranges.length);
+    let min = ranges[i][0].charCodeAt(0),
         max = ranges[i][1].charCodeAt(0);
-    var result = Math.floor(Math.random() * (max - min + 1)) + min;
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
     str += String.fromCharCode(result);
   }
   return str;
