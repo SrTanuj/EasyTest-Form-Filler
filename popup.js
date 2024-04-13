14a068ab691d0f74b9bc9b5b36652ff77a1d88f7
Tanuj Jain <rowdytanuj18@gmail.com>
2024-04-13 17:32:19 +0530

    updated logo styling and code formatting

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const fillButton = document.querySelector('.easyTest-button');
 
 document.addEventListener('DOMContentLoaded', async () => {
   fillButton.addEventListener('click', async () => {
-		let tabs = await chrome.tabs.query({active: true, currentWindow: true});
+    let tabs = await chrome.tabs.query({active: true, currentWindow: true});
     chrome.tabs.sendMessage(tabs[0].id, {action: 'fillForm'});
   });
 });
